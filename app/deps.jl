09b42c611fc5145329d7c15ab0bba2f80aa415ad
fargoo <felipe.c.argolo@protonmail.com>
2019-06-21 23:07:52 -0300

    Interact + Gadfly + Distributions

--- app/deps.jl
+++ app/deps.jl
@@ -2,3 +2,6 @@ using Pkg
 Pkg.update()  
 Pkg.add("Mux")
 Pkg.add("Interact")
+Pkg.add("Gadfly")
+Pkg.add("Interact")
+Pkg.add("Distributions")
--- app/server.jl
+++ app/server.jl
@@ -1,13 +1,21 @@
 using Interact, Mux
+using Gadfly, Distributions
+
+ui_dist = @manipulate for α in 1:100, β = 1:100
+    plot(x -> pdf(Beta(α, β), x), 0, 1)
+end
+@layout! ui_dist dom"div"(observe(_),:α,:β)
 
 loadbutton = filepicker()
 hellobutton = button("Hello!")
 goodbyebutton = button("Good bye!")
-ui = vbox(loadbutton,hbox(hellobutton, goodbyebutton))
+
+ui_up = vbox(loadbutton,hbox(hellobutton,goodbyebutton))
 
 @app abu = (
     Mux.defaults,
-    page("/",req -> ui),
+    page("/", req -> ui_dist),
+    page("/upload", req -> ui_up),
     Mux.notfound())
 
 Mux.serve(abu)
