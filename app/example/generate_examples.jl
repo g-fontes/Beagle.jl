4f3875a732f1b76a1d8b8a23100f886465136b81
fmacedof <fdemacedof@gmail.com>
2019-07-01 12:13:06 -0300

    exploratory, transformaic function

---
+++ app/example/generate_examples.jl
@@ -0,0 +1,16 @@
+using Distributions, DataFrames, CSV
+
+x_o = collect(-5:.1:5)
+x_positive = collect(0:.05:5)
+y_1 = 2*x_o .+ .5
+y_2 = x_o.^2 .+ .3*x_o .+ .5
+y_3 = .5*x_o.^3 .+ 2*x_o.^2 .- .5*x_o
+y_exp = exp.(x_o)
+y_log = log.(x_positive)
+y_sqrt = sqrt.(x_positive)
+
+df = DataFrame(x = x_o, y_linear = y_1, y_quadratic = y_2,
+               y_cubic = y_3, y_exp = y_exp,
+               y_log = y_log, y_sqrt = y_sqrt)
+
+CSV.write("example/df_example.csv",df)
--- app/exploratory.jl
+++ app/exploratory.jl
@@ -1,9 +1,57 @@
 using CSV
 using Plots
 using Distributions
+using GLM, StatsBase
 
+include("example/generate_examples.jl")
 df = CSV.read("example/df_example.csv")
 
-scatter(df[1,],df[2,])
-scatter!(df[1,],df[3,])
-scatter!(df[1,],df[4,])
+xlims!(-5,5)
+scatter(df[:x,],df[:y_linear],lab="linear")
+scatter!(df[:x,],df[:y_quadratic],lab="quadratic")
+scatter!(df[:x,],df[:y_cubic],lab="cubic")
+scatter!(df[:x,],df[:y_exp],lab="exp")
+scatter!(df[:x,],df[:y_log],lab="log")
+scatter!(df[:x,],df[:y_sqrt],lab="sqrt")
+ylims!(-5,15)
+
+# TransformAIC
+# Given x and y, Calculate:
+# StatsBase::aic
+# StatsBase::r2(obj,variant=:Nagelkerke)
+# using every transform function
+
+transforms =  ["linear","quadratic","cubic","exp","log","'sqrt"]
+
+function TransformAIC(x,y,transforms)
+
+    df = DataFrame(x = x, y = y)
+    trans = DataFrame(transforms = transforms,
+                         AIC = repeat(NaN,length(transforms)),
+                         r2 = repeat(NaN,length(transforms)))
+    for t in transforms
+        if t == "linear"
+            model = lm(@formula(y ~ x),df)
+            trans.AIC[:linear] = aic(model)
+            trans.r2[:linear] = r2(model)
+        end
+        if t == "quadratic"
+            model = lm(@formula(y ~ x),df)
+            trans.AIC[:linear] = aic(model)
+            trans.r2[:linear] = r2(model)
+        end
+        if t == "linear"
+            trans.AIC[:linear] = aic(lm(@formula(y_linear ~ exp(x)),df)))
+        end
+        if t == "linear"
+            trans.AIC[:linear] = aic(lm(@formula(y_linear ~ exp(x)),df)))
+        end
+        if t == "linear"
+            trans.AIC[:linear] = aic(lm(@formula(y_linear ~ exp(x)),df)))
+        end
+        if t == "linear"
+            trans.AIC[:linear] = aic(lm(@formula(y_linear ~ exp(x)),df)))
+        end
+    end
+
+end
